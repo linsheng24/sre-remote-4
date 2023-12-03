e4406abf03a92c71f8837422cc3ca7d3d551a8d8
linsheng24 <linsheng24@gmail.com>
2023-12-04 01:17:31 +0800

    complete cron job

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,12 +1,28 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import * as moment from 'moment';
+import prisma from 'prisma/db';
 
 @Injectable()
 export class CronService {
   private readonly logger = new Logger(CronService.name);
 
-  @Cron('45 * * * * *')
-  handleCron() {
-    this.logger.debug('Called when the current second is 45');
+  @Cron('* 0,8,16 * * *')
+  async handleCron() {
+    const result = await prisma.shipping.groupBy({
+      by: 'status',
+      _count: {
+        status: true,
+      },
+    });
+    console.log({
+      created_dt: moment().format('YYYY-MM-DDTHH:mm:ss[Z]'),
+      trackingSummary: result.reduce((acc, item) => {
+        return {
+          ...acc,
+          [item.status]: item._count.status,
+        };
+      }, {}),
+    });
   }
 }
