7ae22902e31dad015299f1328149e8cec7deb7f5
linsheng24 <linsheng24@gmail.com>
2023-12-14 00:29:26 +0800

    add redis micro service

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice({
+    name: 'CACHE_SERVICE',
+    transport: Transport.REDIS,
+    options: {
+      host: 'localhost',
+      port: 6379,
+    },
+  });
 
   const config = new DocumentBuilder()
     .setTitle('SRE Remote 4 Doc')
@@ -12,6 +22,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
