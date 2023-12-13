9b09a9e96be554b51788e1640aa003a045f55198
linsheng24 <linsheng24@gmail.com>
2023-12-14 01:17:43 +0800

    fix dotenv

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Transport } from '@nestjs/microservices';
-import dotenv from 'dotenv';
-
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
